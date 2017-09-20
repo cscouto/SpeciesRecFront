cf273d7bf8c9325527cc2a9e18f532668c25fcf3
TIAGO-LAPTOP\Tiago <cs.couto@outlook.com>
2017-09-20 00:19:48 -0300

    ajustes

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,7 +2,6 @@ var React = require('react');
 var ListItem = require('./ListItem.jsx');
 var HTTP = require('../services/httpservice');
 
-<<<<<<< HEAD
 var List = React.createClass({
     getInitialState: function() {
         return {items: []};
@@ -18,18 +17,6 @@ var List = React.createClass({
             return <ListItem key={item} text={item} />;
         });
         return (<u>{listItems}</u>);
-=======
-var ingredients = [{"id":1,"text":"ham"}, {"id":2,"text":"cheese"},{"id":3,"text":"potatoes"}];
-
-var List = React.createClass({
-    render: function() {
-        var listItems = ingredients.map(function(item) {
-            return <ListItem key={item.id} ingredient={item.text} />;
-        });
-
-        return (<ul>{listItems}</ul>);
->>>>>>> 86b00d830372c7f90218ad5ab6a9fa517243e347
     }
 });
-
 module.exports = List;
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,25 +1,12 @@
 var React = require('react');
-<<<<<<< HEAD
 
 var ListItem = React.createClass({
     render: function() {
         return (
                 <h4>{this.props.text}</h4>
-=======
-var ListItem = React.createClass({
-    render: function() {
-        return (
-            <li>
-                <h4>{this.props.ingredient}</h4>
-            </li>
->>>>>>> 86b00d830372c7f90218ad5ab6a9fa517243e347
         );
     }
 });
 
-<<<<<<< HEAD
-module.exports = ListItem;
-=======
 module.exports = ListItem;
 
->>>>>>> 86b00d830372c7f90218ad5ab6a9fa517243e347
